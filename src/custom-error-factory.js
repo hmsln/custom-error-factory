141f8ec984df5f0529bd9dd5f1d2eb43ec1b98ec
Hubert <hubert.maslin@gmail.com>
2017-03-09 22:48:31 +0100

    edited comment

--- src/custom-error-factory.js
+++ src/custom-error-factory.js
@@ -41,7 +41,7 @@ var customErrorFactory = function (TypeOfError, name, defaultMessage) {
 		
         /*
         bind stack trace to instance, without capturing the lines where CustomError is defined, so the stack trace
-        stops at the line where custom error is thrown (test)
+        stops at the line where custom error is thrown (test master)
         */
        	Error.captureStackTrace(this, CustomError);
 	}
